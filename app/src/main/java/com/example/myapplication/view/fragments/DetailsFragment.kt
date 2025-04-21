4cfd533298f5cf67a3d5d8f5d21396e8199298c8
Shxlxpxn <o1_2@mail.ru>
2025-04-21 15:09:19 +0300

    Module42 - done.

--- app/src/main/java/com/example/myapplication/view/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/myapplication/view/fragments/DetailsFragment.kt
@@ -1,39 +1,63 @@
 package com.example.myapplication.view.fragments
 
-import android.app.AlertDialog
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
-import com.example.myapplication.view.MainActivity
 import com.example.myapplication.R
 import com.example.myapplication.data.ApiConstants
 import com.example.myapplication.databinding.FragmentDetailsBinding
 import com.example.myapplication.domain.Film
+import com.example.myapplication.viewmodel.DetailsFragmentViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class DetailsFragment : Fragment() {
-
-    private var _binding: FragmentDetailsBinding? = null
-    private val binding
-        get() = _binding!!
-    private val film: Film? by lazy {
-        arguments?.getParcelable("film")
-    }
+    private lateinit var film: Film
+    private lateinit var binding: FragmentDetailsBinding
+    private val viewModel: DetailsFragmentViewModel by viewModels()
+    private val job = Job()
+    private val scope = CoroutineScope(Dispatchers.IO + job)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+    ): View {
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setFilmsDetails()
+
+        binding.detailsFabFavorites.setOnClickListener {
+            if (!film.isInFavorites) {
+                binding.detailsFabFavorites.setImageResource(R.drawable.favourites_yes)
+                film.isInFavorites = true
+            } else {
+                binding.detailsFabFavorites.setImageResource(R.drawable.favourites)
+                film.isInFavorites = false
+            }
+        }
 
         binding.detailsFabShare.setOnClickListener {
             //Создаем интент
@@ -43,39 +67,144 @@ class DetailsFragment : Fragment() {
             //Кладем данные о нашем фильме
             intent.putExtra(
                 Intent.EXTRA_TEXT,
-                "Check out this film: ${film?.title} \n\n ${film?.description}"
+                "Check out this film: ${film.title} \n\n ${film.description}"
             )
             //УКазываем MIME тип, чтобы система знала, какое приложения предложить
             intent.type = "text/plain"
             //Запускаем наше активити
             startActivity(Intent.createChooser(intent, "Share To:"))
         }
+
+        binding.detailsFabDownloadWp.setOnClickListener {
+            performAsyncLoadOfPoster()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 
     private fun setFilmsDetails() {
-        film?.let { film ->
-            Glide.with(this)
-                .load(ApiConstants.IMAGES_URL + "w780" + film.poster)
-                .centerCrop()
-                .into(binding.detailsPoster)
-            with(binding) {
-                detailsToolbar.title = film.title
-                detailsDescription.text = film.description
-                detailsFabFavorites.setImageResource(
-                    if (film.isInFavorites) R.drawable.favourites_yes
-                    else R.drawable.favourites)
-                detailsFabFavorites.setOnClickListener {
-                    if (!film.isInFavorites) {
-                        (requireActivity() as? MainActivity)?.addFilmToFavourites(film)
-                        detailsFabFavorites.setImageResource(R.drawable.favourites_yes)
-                        film.isInFavorites = true
-                    } else {
-                        (requireActivity() as? MainActivity)?.removeFilmToFavourites(film)
-                        detailsFabFavorites.setImageResource(R.drawable.favourites)
-                        film.isInFavorites = false
-                    }
-                }
+        //Получаем наш фильм из переданного бандла
+        film = arguments?.get("film") as Film
+
+        //Устанавливаем заголовок
+        binding.detailsToolbar.title = film.title
+        //Устанавливаем картинку
+        Glide.with(this)
+            .load(ApiConstants.IMAGES_URL + "w780" + film.poster)
+            .centerCrop()
+            .into(binding.detailsPoster)
+        //Устанавливаем описание
+        binding.detailsDescription.text = film.description
+
+        binding.detailsFabFavorites.setImageResource(
+            if (film.isInFavorites) R.drawable.favourites_yes
+            else R.drawable.favourites
+        )
+    }
+
+    private fun performAsyncLoadOfPoster() {
+        //Проверяем есть ли разрешение
+        if (!checkPermission()) {
+            //Если нет, то запрашиваем и выходим из метода
+            requestPermission()
+            return
+        }
+        //Создаем родительский скоуп с диспатчером Main потока, так как будет взаимодействовать с UI
+        MainScope().launch {
+            //Включаем прогресс бар
+            binding.progressBar.isVisible = true
+            //Создаем через async так как нам нужен результат от работы, то есть Bitmap
+            val job = scope.async {
+                viewModel.loadWallpaper(ApiConstants.IMAGES_URL + "original" + film.poster)
             }
-        } ?: AlertDialog.Builder(requireContext()).show()
+            //Сохраняем в галерею, как только файл загрузится
+            saveToGallery(job.await())
+            //Выводим снекбар с кнопкой перейти в галерею
+            Snackbar.make(
+                binding.root,
+                R.string.downloaded_to_gallery,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(R.string.open) {
+                    val intent = Intent()
+                    intent.action = Intent.ACTION_VIEW
+                    intent.type = "image/*"
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+                .show()
+
+            //Отключаем прогресс бар
+            binding.progressBar.isVisible = false
+        }
+    }
+
+    //Узнаем, было ли получено разрешение ранее
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+    //Запрашиваем разрешение
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1
+        )
+    }
+
+    private fun saveToGallery(bitmap: Bitmap) {
+        //Проверяем версию системы
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //Создаем объект для передачи данных
+            val contentValues = ContentValues().apply {
+                //Составляем информацию для файла(имя, тип, дата создания, куда сохранять и т.д.)
+                put(MediaStore.Images.Media.TITLE, film.title.handleSingleQuote())
+                put(
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    film.title.handleSingleQuote()
+                )
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(
+                    MediaStore.Images.Media.DATE_ADDED,
+                    System.currentTimeMillis() / 1000
+                )
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/FilmsSearchApp")
+            }
+            //Получаем ссылку на объект Content resolver, которые помогает передвать информацию из приложения во вне
+            val contentResolver = requireActivity().contentResolver
+            val uri = contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            //Открываем канал для записи на диск
+            val outputStream = contentResolver.openOutputStream(uri!!)
+            uri.let {
+                contentResolver.openOutputStream(it)?.use { outputStream ->
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                }
+            }            //Закрываем поток
+            outputStream?.close()
+        } else {
+            //Тоже, но для более старых версий ОС
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.insertImage(
+                requireActivity().contentResolver,
+                bitmap,
+                film.title.handleSingleQuote(),
+                film.description.handleSingleQuote()
+            )
+        }
+    }
+
+    private fun String.handleSingleQuote(): String {
+        return this.replace("'", "")
     }
 }
